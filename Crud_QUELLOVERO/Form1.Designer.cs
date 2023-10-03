ea8ef2fa9d4a3080a2b6e0db03054053aa4e5289
Nico22F <nicofabbricatore22@gmail.com>
2023-10-03 11:22:30 +0200

    Aggiungere i file di progetto.

---
+++ Crud_QUELLOVERO/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Crud_QUELLOVERO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titolo_prodotto = new System.Windows.Forms.Label();
+            this.text_nome = new System.Windows.Forms.TextBox();
+            this.titolo_prezzo = new System.Windows.Forms.Label();
+            this.text_prezzo = new System.Windows.Forms.TextBox();
+            this.create = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.titolo = new System.Windows.Forms.Label();
+            this.ConfermaModifica = new System.Windows.Forms.Button();
+            this.mostra = new System.Windows.Forms.Button();
+            this.modifica = new System.Windows.Forms.Button();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titolo_prodotto
+            // 
+            this.titolo_prodotto.AutoSize = true;
+            this.titolo_prodotto.Location = new System.Drawing.Point(12, 9);
+            this.titolo_prodotto.Name = "titolo_prodotto";
+            this.titolo_prodotto.Size = new System.Drawing.Size(127, 13);
+            this.titolo_prodotto.TabIndex = 0;
+            this.titolo_prodotto.Text = "NOME DEL PRODOTTO";
+            // 
+            // text_nome
+            // 
+            this.text_nome.Location = new System.Drawing.Point(15, 25);
+            this.text_nome.Name = "text_nome";
+            this.text_nome.Size = new System.Drawing.Size(160, 20);
+            this.text_nome.TabIndex = 1;
+            // 
+            // titolo_prezzo
+            // 
+            this.titolo_prezzo.AutoSize = true;
+            this.titolo_prezzo.Location = new System.Drawing.Point(12, 57);
+            this.titolo_prezzo.Name = "titolo_prezzo";
+            this.titolo_prezzo.Size = new System.Drawing.Size(139, 13);
+            this.titolo_prezzo.TabIndex = 2;
+            this.titolo_prezzo.Text = "PREZZO DEL PRODOTTO";
+            // 
+            // text_prezzo
+            // 
+            this.text_prezzo.Location = new System.Drawing.Point(15, 73);
+            this.text_prezzo.Name = "text_prezzo";
+            this.text_prezzo.Size = new System.Drawing.Size(160, 20);
+            this.text_prezzo.TabIndex = 3;
+            // 
+            // create
+            // 
+            this.create.Location = new System.Drawing.Point(15, 130);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(160, 26);
+            this.create.TabIndex = 4;
+            this.create.Text = "Aggiungi un prodotto alla lista";
+            this.create.UseVisualStyleBackColor = true;
+            this.create.Click += new System.EventHandler(this.create_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(610, 31);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(178, 407);
+            this.lista.TabIndex = 5;
+            this.lista.SelectedIndexChanged += new System.EventHandler(this.lista_SelectedIndexChanged);
+            // 
+            // titolo
+            // 
+            this.titolo.AutoSize = true;
+            this.titolo.Location = new System.Drawing.Point(607, 9);
+            this.titolo.Name = "titolo";
+            this.titolo.Size = new System.Drawing.Size(29, 13);
+            this.titolo.TabIndex = 6;
+            this.titolo.Text = "titolo";
+            // 
+            // ConfermaModifica
+            // 
+            this.ConfermaModifica.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.ConfermaModifica.Location = new System.Drawing.Point(181, 43);
+            this.ConfermaModifica.Name = "ConfermaModifica";
+            this.ConfermaModifica.Size = new System.Drawing.Size(142, 40);
+            this.ConfermaModifica.TabIndex = 7;
+            this.ConfermaModifica.Text = "Conferma modifica";
+            this.ConfermaModifica.UseVisualStyleBackColor = true;
+            this.ConfermaModifica.Click += new System.EventHandler(this.ConfermaModifica_Click);
+            // 
+            // mostra
+            // 
+            this.mostra.Location = new System.Drawing.Point(15, 162);
+            this.mostra.Name = "mostra";
+            this.mostra.Size = new System.Drawing.Size(160, 23);
+            this.mostra.TabIndex = 8;
+            this.mostra.Text = "Visualizza i prodotti della lista";
+            this.mostra.UseVisualStyleBackColor = true;
+            this.mostra.Click += new System.EventHandler(this.mostra_Click);
+            // 
+            // modifica
+            // 
+            this.modifica.Location = new System.Drawing.Point(15, 191);
+            this.modifica.Name = "modifica";
+            this.modifica.Size = new System.Drawing.Size(160, 20);
+            this.modifica.TabIndex = 9;
+            this.modifica.Text = "Modifica un prodotto nella lista";
+            this.modifica.UseVisualStyleBackColor = true;
+            this.modifica.Click += new System.EventHandler(this.modifica_Click);
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(15, 217);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(159, 21);
+            this.delete_button.TabIndex = 10;
+            this.delete_button.Text = "Elimina un prodotto dalla lista";
+            this.delete_button.UseVisualStyleBackColor = true;
+            this.delete_button.Click += new System.EventHandler(this.delete_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.modifica);
+            this.Controls.Add(this.mostra);
+            this.Controls.Add(this.ConfermaModifica);
+            this.Controls.Add(this.titolo);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.text_prezzo);
+            this.Controls.Add(this.titolo_prezzo);
+            this.Controls.Add(this.text_nome);
+            this.Controls.Add(this.titolo_prodotto);
+            this.Name = "Form1";
+            this.Text = "Finestra22F";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titolo_prodotto;
+        private System.Windows.Forms.TextBox text_nome;
+        private System.Windows.Forms.Label titolo_prezzo;
+        private System.Windows.Forms.TextBox text_prezzo;
+        private System.Windows.Forms.Button create;
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Label titolo;
+        private System.Windows.Forms.Button ConfermaModifica;
+        private System.Windows.Forms.Button mostra;
+        private System.Windows.Forms.Button modifica;
+        private System.Windows.Forms.Button delete_button;
+    }
+}
+
---
+++ Crud_QUELLOVERO/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Crud_QUELLOVERO
+{
+    public partial class Form1 : Form
+    {
+        // structure
+
+        public struct prodotto
+        {
+            public string nome;
+            public float prezzo;
+        }
+        public prodotto[] p = new prodotto[100];
+        public int dim;
+        public Form1()
+        {
+            InitializeComponent();
+            dim = 0;
+            lista.Visible = false;
+            titolo.Visible = false;
+            ConfermaModifica.Visible = false;
+        }
+
+        // variabili booleane per modificare / eliminare i prodotti
+
+        public bool conferma_modifica = false;
+        public bool conferma_elimina = false;
+
+        // funzione per aggiungere i prodotti 
+
+        // mostra prodotti
+
+        public void visualizza(prodotto[] pp)
+        {
+            lista.Items.Clear();
+            for (int i = 0; i < dim; i++)
+            {
+                lista.Items.Add(prodString(p[i]));
+            }
+        }
+
+        // funzione che scrive la stringa
+        public string prodString(prodotto p)
+        {
+            return $"{p.nome}:{p.prezzo}";
+        }
+
+        // bottone che aggiunge un prodotto alla lista
+        private void create_Click(object sender, EventArgs e)
+        {
+            // mostrare anche le text box solo se cliccato
+            // controllo input corretto
+
+
+            p[dim].nome = text_nome.Text;
+            p[dim].prezzo = float.Parse(text_prezzo.Text);
+            dim++;
+            visualizza(p);
+            text_nome.Text = "";
+            text_prezzo.Text = "";
+            MessageBox.Show("Prodotto Aggiunto!");
+
+        }
+        // bottone che mostra gli elementi della lista (mostra direttamente la lista)
+        private void mostra_Click(object sender, EventArgs e)
+        {
+            if (lista.Visible == false)
+            {
+                lista.Visible = true;
+                titolo.Visible = true;
+            }
+            else
+            {
+                lista.Visible = false;
+                titolo.Visible = false;
+            }
+        }
+
+        // bottone che modifica un prodotto
+        private void modifica_Click(object sender, EventArgs e)
+        {
+            // mostra lista prodotti
+
+            lista.Visible = true;
+            conferma_modifica = true;
+            conferma_elimina = false; // può essere attivata una sola funzione
+        }
+
+        // split
+        static string[] Split(string stringa)
+        {
+            string[] array = new string[2];
+            string frase = "";
+            int p = 0;
+            for (int i = 0; i < stringa.Length; i++)
+            {
+                if (stringa[i] == ':')
+                {
+                    array[p] = frase; p++; frase = "";
+                }
+                else
+                {
+                    frase += stringa[i];
+                }
+                if (i == stringa.Length - 1)
+                {
+                    array[p] = frase;
+                }
+            }
+            return array;
+        }
+
+        string nomignolo;
+        float prezzolo;
+
+        // modificare / eliminare un prodotto
+        private void lista_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // modifica prodotto
+
+            if (conferma_modifica == true)
+            {
+                int scelta = lista.SelectedIndex;
+                MessageBox.Show("Procedi con la modifica del prodotto da lei selezionato (per continuare clicca il nome del prodotto che vuoi modificare nella lista prodotti)");
+                string oggettomodifca = lista.Items[scelta].ToString();
+                string[] modificona = Split(oggettomodifca);
+                text_nome.Text = modificona[0];
+                text_prezzo.Text = modificona[1];
+                ConfermaModifica.Visible = true;
+                nomignolo = text_nome.Text;
+                prezzolo = float.Parse(text_prezzo.Text);
+            }
+            // elimina prodotto
+            if (conferma_elimina == true)
+            {
+                // messagebox con input per confermare la scelta
+                int scelta = lista.SelectedIndex;
+                string oggettoDAELIMINARE = lista.Items[scelta].ToString();
+                // messaggio                                                        // titolo
+                DialogResult inputbox = MessageBox.Show($"Vuoi eliminare il prodotto selezionato? ({oggettoDAELIMINARE})", "ELIMINAZIONE PRODOTTI", MessageBoxButtons.YesNo);
+                switch (inputbox)
+                {
+                    // se l'utente ha cliccato di si
+                    case DialogResult.Yes:
+                        // eliminazione prodotto dalla struct
+                        lista.Items.Clear();
+                        string[] modificona = Split(oggettoDAELIMINARE);
+
+                        for (int i = 0; i < dim; i++)
+                        {
+                            if (modificona[0] == p[i].nome)
+                            {
+                                for (int j = i, a =j+1; j < dim; j++, a++)
+                                {
+                                    p[j].nome = p[a].nome;
+                                    p[j].prezzo = p[a].prezzo;
+                                   
+                                }
+                                dim--;
+                                break;
+                            }
+                        }
+
+                        // elimino lo spazio aggiuntivo con resize
+
+                        Array.Resize(ref p, p.Length - 1);
+
+                        // riaggiorno la lista
+
+                        visualizza(p);
+
+                        MessageBox.Show("Prodotto eliminato con succcesso");
+
+                        break;
+                    // se l'utente ha cliccato no
+                    case DialogResult.No:
+                        break;
+                }
+            }
+        }
+
+        private void ConfermaModifica_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < dim; i++)
+            {
+                if (p[i].nome == nomignolo)
+                {
+                    p[i].prezzo = float.Parse(text_prezzo.Text);
+                    p[i].nome = text_nome.Text;
+                    break;
+                }
+            }
+            visualizza(p);
+            text_nome.Text = "";
+            text_prezzo.Text = "";
+            MessageBox.Show("Prodotto Aggiornato!");
+            ConfermaModifica.Visible = false;
+        }
+
+        private void delete_button_Click(object sender, EventArgs e)
+        {
+            lista.Visible = true;
+            conferma_elimina = true;
+            conferma_modifica = false;
+            MessageBox.Show("Procedi con l'eliminazione del prodotto! (per continuare clicca il nome del prodotto che vuoi eliminare nella lista prodotti)");
+        }
+    }
+}
